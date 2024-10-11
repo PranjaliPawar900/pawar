92f1f3623ac175ec10e70e543b9feac05bed5863
PranjaliPawar900 <140233499+PranjaliPawar900@users.noreply.github.com>
2024-10-11 20:39:04 +0530

    DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+package pranjali;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 {
+		     {
+		        int a = 10, b = 5;
+
+		        int sum = a + b;
+		        int difference = a - b;
+		        int product = a * b;
+		        int quotient = a / b;
+		        int remainder = a % b;
+
+		        System.out.println("Sum: " + sum);
+		        System.out.println("Difference: " + difference);
+		        System.out.println("Product: " + product);
+		        System.out.println("Quotient: " + quotient);
+		        System.out.println("Remainder: " + remainder);
+		    }
+		}
+	}
+
+}
